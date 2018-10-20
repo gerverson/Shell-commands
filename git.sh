5c46b64abc0b6c7bd82e574fea4a8eec7170ffb5
Gerverson Araujo <gerversonsa@gmail.com>
2018-10-20 11:57:41 -0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,2 +1,16 @@
 !#/bin/bash
-git clone
+
+#Iniciar git na pasta
+git init
+
+#Adiconando arquivo ao git
+git add README.md
+
+#Gerando Commit
+git commit -m "first commit"
+
+#Sicronizando com GitHub
+git remote add origin https://github.com/gerverson/docker.git
+
+#Subindo para o github
+git push -u origin master
