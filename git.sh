fdb1f63e6633066d8c145b32e20fb48ed2d1806c
Gerverson Araujo <gerversonsa@gmail.com>
2018-12-17 16:50:22 -0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -17,3 +17,9 @@ git push -u origin master
 
 #Desfazer alterações nos arquivos modificados
 git checkout -f
+
+
+#Desfazer ultimo commit
+git reset HEAD~1 --hard # elimina as atividades feitas no stage. Obs. O número 1 indica a quantidade de commites que serão apagados.
+git reset HEAD~1 --soft # voltar com as atividades ao stage
+git push origin -f
